f3cb6edb6bf90834bba92facabce5ca9ae9ae481
MercerMorning <hugopochta@gmail.com>
2020-08-02 21:56:19 +0300

    'commit'

---
+++ index.js
@@ -0,0 +1,16 @@
+let arr = ['я', 'умею', 'писать', 'рекурсивные', 'функции'];
+
+function consoleRec(arr, count)
+{
+    function getElements(count) {
+        console.log(arr[count])
+    }
+    getElements(count++);
+    if (count < arr.length) {
+        return consoleRec(arr, count++);
+    }
+
+}
+
+
+consoleRec(arr, 0)
